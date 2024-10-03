e08936a4c2d119c16cfce0427290d752a8036fef
Vehbi <126095007+vehbiu@users.noreply.github.com>
2024-10-03 07:22:45 -0500

    Add item l ist

--- components/item-list.tsx
+++ components/item-list.tsx
@@ -13,8 +13,8 @@ export default function ItemList({ title, items }: ItemListProps) {
       <div className="flex flex-col justify gap-2">
       <h3 className="text-xl font-bold tracking-tight">{title}</h3>
       <div className="flex flex-row justify-stretch items-center gap-2 flex-wrap ms-2">
-        {items.map(({ name, url }) => (
-          <a className="text-gray-500 hover:text-blue-500 hover:underline transition-all duration-300" href={url} style={{
+        {items.map(({ name, url }, i) => (
+          <a key={"item-list-" + i} className="text-gray-500 hover:text-blue-500 hover:underline transition-all duration-300" href={url} style={{
             // glow around the text (not only border)
             textShadow: `0 0 10px rgba(59, 130, 246, 0.5)`,
             transition: 'text-shadow 0.2s'
