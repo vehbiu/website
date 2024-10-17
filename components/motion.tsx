4823d2cb4126e6d8f45b96333bc43b910fedaf07
Vehbi <126095007+vehbiu@users.noreply.github.com>
2024-10-17 18:57:12 -0500

    add nice hero svg

--- components/motion.tsx
+++ components/motion.tsx
@@ -8,6 +8,7 @@ export const MotionDiv = motion.div;
 export const MotionH1 = motion.h1;
 export const MotionH2 = motion.h2;
 
+export const MotionSVG = motion.svg;
 export const MotionA = motion.a;
 export const MotionP = motion.p;
 export const MotionSpan = motion.span;
