d7f62b762e1cbb0f5e5db71cbfed18a8f67412dd
Vehbi <126095007+vehbiu@users.noreply.github.com>
2024-10-16 20:21:06 -0500

    Set serverActions allowedOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["vehbi.me", "*.pages.dev"],
+        }
+    }
+};
 
 export default nextConfig;
